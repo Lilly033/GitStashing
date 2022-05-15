284f451dd8dba1e88732e3669c83e7a8bc7f550f
Lilly033 <ljiljanajevtinijevic@gmail.com>
2022-05-15 09:05:27 -0700

    The red Audii car is ready with some basic features

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -3,8 +3,12 @@ package com.syntax;
 public class Main {
 
     public static void main(String[] args) {
-        basicFeatures car = new basicFeatures();
-        
-	// write your code here
+        basicFeatures Auddi = new basicFeatures();
+        Auddi.Color="red";
+        Auddi.doors="4";
+        Auddi.engineCapac="V8";
+        Auddi.headlights="blue";
+        Auddi.regNo="tuzik";
+
     }
 }
